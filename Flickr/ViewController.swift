ce4e0e444c78a27efd9c8f6e941682ab07d46ea8
Matheus Sousa <matheusvds@gmail.com>
2020-09-21 20:22:49 -0300

    adds the initial project

--- Flickr/ViewController.swift
+++ Flickr/ViewController.swift
@@ -1,20 +1,4 @@
-//
-//  ViewController.swift
-//  Flickr
-//
-//  Created by Matheus Vasconcelos de Sousa on 21/09/20.
-//  Copyright © 2020 Matheus Vasconcelos de Sousa. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
+class ViewController: UIViewController {}
 
