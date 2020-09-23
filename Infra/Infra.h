64cdc920163dc233e7385543bee9c7f963f5687b
Matheus Sousa <matheusvds@gmail.com>
2020-09-22 22:10:53 -0300

    creates a target for the infra layer

---
+++ Infra/Infra.h
@@ -0,0 +1,19 @@
+//
+//  Infra.h
+//  Infra
+//
+//  Created by Matheus Vasconcelos de Sousa on 22/09/20.
+//  Copyright © 2020 Matheus Vasconcelos de Sousa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Infra.
+FOUNDATION_EXPORT double InfraVersionNumber;
+
+//! Project version string for Infra.
+FOUNDATION_EXPORT const unsigned char InfraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Infra/PublicHeader.h>
+
+
