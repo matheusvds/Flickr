6e26324c66651c2ea3352b7370d6325f2835eca0
Matheus Sousa <matheusvds@gmail.com>
2020-09-23 19:18:25 -0300

    adds UI and application layers

---
+++ UI/UI.h
@@ -0,0 +1,19 @@
+//
+//  UI.h
+//  UI
+//
+//  Created by Matheus Vasconcelos de Sousa on 23/09/20.
+//  Copyright © 2020 Matheus Vasconcelos de Sousa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UI.
+FOUNDATION_EXPORT double UIVersionNumber;
+
+//! Project version string for UI.
+FOUNDATION_EXPORT const unsigned char UIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UI/PublicHeader.h>
+
+
