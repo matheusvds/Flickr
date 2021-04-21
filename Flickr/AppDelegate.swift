4603e93c5afd6b2945c029f1db0743191e0b9dfa
Matheus Sousa <msousa@dunelm.com>
2021-04-21 14:41:45 +0100

    remove unnecessary window attribution in the appdelegate

--- Flickr/AppDelegate.swift
+++ Flickr/AppDelegate.swift
@@ -12,12 +12,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let getPhotos = RemoteGetPhotos(searchPhotos: searchPhotos, getSizes: getSizes)
         let getImageData = RemoteGetImageData(client: client)
         let main = Main(getImageData: getImageData, getPhotos: getPhotos)
-        main.window = self?.window
         return main
     }()
-    
-    var window: UIWindow?
-    
+        
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         app.start()
         return true
